328ccd52c2ac8b4a8526675a8ecf993a23aac7ea
bbozkurt15 <bbozkurt15@login03.kuacc.ku.edu.tr>
2023-08-02 16:59:57 +0300

    checkpoint

---
+++ Simulations/Classification/FeedbackAlignment/FAMLP_MNIST_3Layers_Simulation_v1.py
@@ -0,0 +1,110 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import torchvision
+import torch.nn.functional as F
+import argparse
+import matplotlib
+import pandas as pd
+
+from tqdm import tqdm
+import glob
+import os
+from datetime import datetime
+from itertools import product
+import time
+import math
+from torch.autograd import Variable
+from biotorch.module.biomodule import BioModule
+import sys
+sys.path.append("./src")
+from ANN import *
+from torch_utils import *
+
+import warnings
+warnings.filterwarnings("ignore")
+
+file_path = os.path.realpath(__file__)
+working_path = os.path.abspath(os.path.dirname(__file__))
+os.chdir(working_path)
+# print(os.getcwd())
+
+if not os.path.exists("../Results"):
+    os.mkdir("../Results")
+
+pickle_name_for_results = "simulation_results_FeedbackAlignment_MNIST_3Layers_V1.pkl"
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), 
+                                            torchvision.transforms.Normalize(mean=(0.0,), std=(1.0,))])
+
+mnist_dset_train = torchvision.datasets.MNIST('../../../data', train=True, transform=transform, target_transform=None, download=True)
+train_loader = torch.utils.data.DataLoader(mnist_dset_train, batch_size=20, shuffle=True, num_workers=0)
+
+mnist_dset_test = torchvision.datasets.MNIST('../../../data', train=False, transform=transform, target_transform=None, download=True)
+test_loader = torch.utils.data.DataLoader(mnist_dset_test, batch_size=20, shuffle=False, num_workers=0)
+
+activation = F.relu
+architecture = [784, 500, 500, 10]
+
+RESULTS_DF = pd.DataFrame( columns = ['setting_number', 'seed', 'Model', 'Hyperparams', 'Trn_ACC_list', 'Tst_ACC_list'])
+
+lr_list = [5*1e-4]
+lr_decay_gamma_list = [0.95, 0.9]
+lr_decay_scheduler_step_list = [10, 25]
+optimizer_type_list = ["Adam", "SGD"]
+final_layer_activation_list = [False, True]
+n_epochs = 50
+seed_list = [10*j for j in range(10)]
+
+setting_number = 0
+for lr, lr_decay, lr_decay_step, optimizer_type, final_layer_activation_ in product(lr_list, lr_decay_gamma_list, lr_decay_scheduler_step_list, optimizer_type_list, final_layer_activation_list):
+
+    setting_number += 1
+    hyperparams_dict = {"lr": lr, "lr_decay": lr_decay, "lr_decay_step": lr_decay_step, "optimizer_type": optimizer_type,
+                        "final_layer_activation": final_layer_activation_, "architecture" : architecture}
+    for seed_ in seed_list:
+        np.random.seed(seed_)
+        torch.manual_seed(seed_)
+
+        trn_acc_list = []
+        tst_acc_list = []
+
+        criterion = torch.nn.MSELoss().to(device)
+        model = BioModule(MLP(architecture, activation = activation, final_layer_activation = final_layer_activation_), mode = "fa").to(device)
+        if optimizer_type == "Adam":
+            optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        elif optimizer_type == "SGD":
+            optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=0.001)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=lr_decay_step, gamma=lr_decay)
+
+        for epoch_ in range(n_epochs):
+            model.train()
+            for idx, (x, y) in tqdm(enumerate(train_loader)):
+                x, y = Variable(x.to(device)), Variable(y.to(device))
+                y_one_hot = F.one_hot(y, num_classes=architecture[-1])
+                optimizer.zero_grad()
+                y_hat = model(x)
+                loss = criterion(y_hat,y_one_hot.to(torch.float32)) # Use this if criterion = torch.nn.MSELoss().to(device)
+                # backward pass: compute gradient of the loss with respect to model parameters
+                model.zero_grad()
+                loss.backward()
+                # perform a single optimization step (parameter update)
+                optimizer.step()
+            
+            scheduler.step()
+            model.eval()
+
+            trn_acc = evaluateClassification(model, train_loader, device, False)
+            tst_acc = evaluateClassification(model, test_loader, device, False)
+            trn_acc_list.append(trn_acc)
+            tst_acc_list.append(tst_acc)
+
+        Result_Dict = {"setting_number" : setting_number, "seed" : seed_, "Model" : "MLP-FeedBackAlignment", 
+                        "Hyperparams" : hyperparams_dict, "Trn_ACC_list" : trn_acc_list, "Tst_ACC_list" : tst_acc_list}
+
+        RESULTS_DF = RESULTS_DF.append(Result_Dict, ignore_index = True)
+        RESULTS_DF.to_pickle(os.path.join("../Results", pickle_name_for_results))
+
+RESULTS_DF.to_pickle(os.path.join("../Results", pickle_name_for_results))
--- src/ANN.py
+++ src/ANN.py
@@ -2,6 +2,9 @@ import torch
 import torchvision
 import torch.nn.functional as F
 from torch_utils import *
+import torch.nn as nn
+from torch import autograd
+from torch.autograd import Variable
 
 class MLP(torch.nn.Module):
     """
@@ -31,3 +34,160 @@ class MLP(torch.nn.Module):
             x = self.linear_layers[-1](x)
             
         return x
+
+class MLP_V2(torch.nn.Module):
+    """
+    Multi Layer Perceptron
+    """
+    def __init__(self, architecture, activation = F.relu, final_layer_activation = None):
+        super(MLP, self).__init__()
+        
+        self.activation = activation
+        self.architecture = architecture 
+        self.nc = self.architecture[-1]
+        self.final_layer_activation = final_layer_activation
+        
+        self.linear_layers = torch.nn.ModuleList()
+        for idx in range(len(architecture)-1):
+            m = torch.nn.Linear(architecture[idx], architecture[idx+1], bias=True)
+            self.linear_layers.append(m)
+            
+    def forward(self, x):
+        x = x.view(x.size(0),-1) # flattening the input
+        for idx in range(len(self.architecture)-2):
+            x = self.activation(self.linear_layers[idx](x))
+            
+        if self.final_layer_activation is not None:
+            x = self.final_layer_activation(self.linear_layers[-1](x))
+        else:
+            x = self.linear_layers[-1](x)
+            
+        return x
+
+class FeedbackAlignmentMLP(nn.Module):
+    def __init__(self, architecture, activation = F.relu, final_layer_activation = None):
+        super(FeedbackAlignmentMLP, self).__init__()
+        self.architecture = architecture
+        self.activation = activation
+        self.final_layer_activation = final_layer_activation
+        self.nc = self.architecture[-1]
+
+        self.linear_layers = torch.nn.ModuleList()
+        for idx in range(len(architecture)-1):
+            m = LinearFAModule(architecture[idx], architecture[idx+1], bias=True)
+            self.linear_layers.append(m)
+
+    def forward(self, x):
+        x = x.view(x.size(0),-1) # flattening the input
+        for idx in range(len(self.architecture)-2):
+            x = self.activation(self.linear_layers[idx](x))
+            
+        if self.final_layer_activation is not None:
+            x = self.final_layer_activation(self.linear_layers[-1](x))
+        else:
+            x = self.linear_layers[-1](x)
+            
+        return x
+
+######################################################
+#### The below code is taken from https://github.com/L0SG/feedback-alignment-pytorch
+######################################################
+class LinearFANetwork(nn.Module):
+    """
+    Linear feed-forward networks with feedback alignment learning
+    Does NOT perform non-linear activation after each layer
+    """
+    def __init__(self, in_features, num_layers, num_hidden_list):
+        """
+        :param in_features: dimension of input features (784 for MNIST)
+        :param num_layers: number of layers for feed-forward net
+        :param num_hidden_list: list of integers indicating hidden nodes of each layer
+        """
+        super(LinearFANetwork, self).__init__()
+        self.in_features = in_features
+        self.num_layers = num_layers
+        self.num_hidden_list = num_hidden_list
+
+        # create list of linear layers
+        # first hidden layer
+        self.linear = [LinearFAModule(self.in_features, self.num_hidden_list[0])]
+        # append additional hidden layers to list
+        for idx in range(self.num_layers - 1):
+            self.linear.append(LinearFAModule(self.num_hidden_list[idx], self.num_hidden_list[idx+1]))
+
+        # create ModuleList to make list of layers work
+        self.linear = nn.ModuleList(self.linear)
+
+    def forward(self, inputs):
+        """
+        forward pass, which is same for conventional feed-forward net
+        :param inputs: inputs with shape [batch_size, in_features]
+        :return: logit outputs from the network
+        """
+
+        # first layer
+        linear1 = self.linear[0](inputs)
+
+        # second layer
+        linear2 = self.linear[1](linear1)
+
+        return linear2
+
+
+class LinearFAFunction(autograd.Function):
+
+    @staticmethod
+    # same as reference linear function, but with additional fa tensor for backward
+    def forward(context, input, weight, weight_fa, bias=None):
+        context.save_for_backward(input, weight, weight_fa, bias)
+        output = input.mm(weight.t())
+        if bias is not None:
+            output += bias.unsqueeze(0).expand_as(output)
+        return output
+
+    @staticmethod
+    def backward(context, grad_output):
+        input, weight, weight_fa, bias = context.saved_variables
+        grad_input = grad_weight = grad_weight_fa = grad_bias = None
+
+        if context.needs_input_grad[0]:
+            # all of the logic of FA resides in this one line
+            # calculate the gradient of input with fixed fa tensor, rather than the "correct" model weight
+            grad_input = grad_output.mm(weight_fa.to(grad_output.device))
+        if context.needs_input_grad[1]:
+            # grad for weight with FA'ed grad_output from downstream layer
+            # it is same with original linear function
+            grad_weight = grad_output.t().mm(input)
+        if bias is not None and context.needs_input_grad[3]:
+            grad_bias = grad_output.sum(0).squeeze(0)
+
+        return grad_input, grad_weight, grad_weight_fa, grad_bias
+
+
+class LinearFAModule(nn.Module):
+
+    def __init__(self, input_features, output_features, bias=True):
+        super(LinearFAModule, self).__init__()
+        self.input_features = input_features
+        self.output_features = output_features
+
+        # weight and bias for forward pass
+        # weight has transposed form; more efficient (so i heard) (transposed at forward pass)
+        self.weight = nn.Parameter(torch.Tensor(output_features, input_features))
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(output_features))
+        else:
+            self.register_parameter('bias', None)
+
+        # fixed random weight and bias for FA backward pass
+        # does not need gradient
+        self.weight_fa = Variable(torch.FloatTensor(output_features, input_features), requires_grad=False)
+
+        # weight initialization
+        torch.nn.init.kaiming_uniform_(self.weight)
+        torch.nn.init.kaiming_uniform_(self.weight_fa)
+        # self.weight_fa *= 0.05
+        torch.nn.init.constant_(self.bias, 1)
+
+    def forward(self, input):
+        return LinearFAFunction.apply(input, self.weight, self.weight_fa, self.bias)
